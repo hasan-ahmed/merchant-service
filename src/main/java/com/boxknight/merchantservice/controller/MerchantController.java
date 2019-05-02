7cd4e29870a8a1d9d2c61c55a58de95e26251c31
Hasan <hasan.mahmed95@gmail.com>
2019-05-02 00:32:09 -0400

    Add a controller and test to verify if the CI/CD pipeline is working

---
+++ src/main/java/com/boxknight/merchantservice/controller/MerchantController.java
@@ -0,0 +1,18 @@
+package com.boxknight.merchantservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/merchants")
+public class MerchantController {
+    @GetMapping
+    public ResponseEntity<?> getAllMerchants() {
+        return ResponseEntity.status(HttpStatus.OK).body("The test is working!");
+    }
+}
+
