3cf917ce1bf06be8e84c6084c62a6a2b41bcb77f
Hasan <hasan.mahmed95@gmail.com>
2019-05-02 14:28:53 -0400

    Added environment variables to Jenkins file

--- src/main/java/com/boxknight/merchantservice/controller/MerchantController.java
+++ src/main/java/com/boxknight/merchantservice/controller/MerchantController.java
@@ -1,5 +1,6 @@
 package com.boxknight.merchantservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/merchants")
 public class MerchantController {
+
+    @Value("${merchant.db}")
+    private String merchantDB;
+
     @GetMapping
     public ResponseEntity<?> getAllMerchants() {
-        return ResponseEntity.status(HttpStatus.OK).body("Testing feature 2!!!!!!");
+        return ResponseEntity.status(HttpStatus.OK).body("Testing feature 2!!!!!!" + merchantDB);
     }
 }
